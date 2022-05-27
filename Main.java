954358631e25d2170abe90d7a847a27d8518acd0
Raja Sekhar Veeramalla <vrm.rajasekhar@gmail.com>
2022-05-28 00:38:30 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.phase1Project;
+
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "E:\\simplilearnProjects\\Phase1Project"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
